db4ca0b09368d8f16d8600a7c96eeca0f4fe255d
Sakshi Mittal <sakshi.mittal@knoldus.com>
2021-03-07 22:03:54 +0530

    Get maximum of two numbers

---
+++ LambdaPractice_Assignment1/src/main/java/com/lambda/kup/GetMaximumMain.java
@@ -0,0 +1,20 @@
+package com.lambda.kup;
+
+public class GetMaximumMain {
+
+        public static void main(String[] args) {
+
+
+                int number1 = 20;
+                int number2 = 4;
+
+                GetMaximum_Interface getMaximum = new GetMaximum();
+                int maxOfTwoNumbers = getMaximum.getMaximumOfTwoNumbers(number1, number2);
+
+                System.out.println("Two numbers are: " + number1 + "," + number2);
+
+                System.out.println("Maximum number is: " + maxOfTwoNumbers);
+
+        }
+
+}
---
+++ LambdaPractice_Assignment1/src/main/java/com/lambda/kup/GetMaximum_Interface.java
@@ -0,0 +1,8 @@
+package com.lambda.kup;
+
+@FunctionalInterface
+public interface GetMaximum_Interface {
+
+        int getMaximumOfTwoNumbers(int numberOne, int numberTwo);
+
+}
